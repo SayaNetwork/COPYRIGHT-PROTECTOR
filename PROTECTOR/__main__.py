a6547ddfa28fa9f95466c71c49610e8e8eccfcaa
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-07-06 10:31:53 +0530

    Update __main__.py

--- PROTECTOR/__main__.py
+++ PROTECTOR/__main__.py
@@ -14,7 +14,7 @@ async def JARVIS():
     print("Bot Started Successfully")
     await idle()
     print("MAI HU PIRO CODER BOLO NHI AAYA ERROR")
-    await PROTECTOR.send_message(LOGGER_ID, "**ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ʏᴏᴜʀ ʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ✅ \n ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ  [malware](https://t.me/ll_malware_ll)**")
+    await PROTECTOR.send_message(LOGGER_ID, "**ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ʏᴏᴜʀ ʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ✅ \n ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ  [Saya](https://t.me/SayaProject)**")
 
 if __name__ == "__main__":
     loop.run_until_complete(JARVIS())
